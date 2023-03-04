59590881846959cf562ab2cbcba45b23198c431b
tran.tuan <tdtuan97@gmail.com>
2023-03-04 15:37:41 +0700

    update git page

--- src/common/historyCommon.js
+++ src/common/historyCommon.js
@@ -2,4 +2,5 @@ import {createBrowserHistory} from 'history';
 
 // A singleton history object for easy API navigation
 const historyCommon = createBrowserHistory();
+//const historyCommon = createBrowserHistory({ basename: '/food-blog-web' });
 export default historyCommon;
